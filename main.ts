701d2d7d68b2625094872464d1772f5c70f539fc
colty8008 <colton.cameron@hartselletigers.org>
2024-11-21 11:38:00 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+input.onButtonPressed(Button.A, function () {
+    record.startRecording(record.BlockingState.Blocking)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        record.playAudio(record.BlockingState.Blocking)
+    }
+})
+basic.forever(function () {
+	
+})
